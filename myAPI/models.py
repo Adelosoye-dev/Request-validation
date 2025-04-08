4de913a3a26ccb2a5d54a11752eec570b7e10a1e
adelosoye-dev <adelosoyeolajumoke9@gmail.com>
2025-04-08 01:42:41 +0100

    updating models.py and managing data base

--- myAPI/models.py
+++ myAPI/models.py
@@ -10,3 +10,15 @@ class Person(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+    
+class Portfolio(models.Model):
+    person = models.OneToOneField(Person, on_delete=models.CASCADE, related_name='portfolio')
+    position = models.CharField(max_length=100)
+    profession = models.CharField(max_length=100)
+    years_of_experience = models.IntegerField()
+    sector = models.CharField(max_length=100)
+    skills = models.JSONField() 
+    
+    def __str__(self):
+        return f"{self.person.first_name}'s Portfolio"
+    
